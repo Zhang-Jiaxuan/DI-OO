9ea8ed24688db9874db4a3a549b7f6a33611ae46
Zhang-Jiaxuan <64740407+Zhang-Jiaxuan@users.noreply.github.com>
2024-02-02 18:00:32 +0800

    Update and rename RL_NCE_FC_new.py to DI-OO_MLP.py

--- DI-OO_MLP.py
+++ DI-OO_MLP.py
@@ -23,15 +23,10 @@ import time
 seed = 1  # seed必须是int，可以自行设置
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)  # 让显卡产生的随机数一致
-torch.cuda.manual_seed_all(seed)  # 多卡模式下，让所有显卡生成的随机数一致？这个待验证
+torch.cuda.manual_seed_all(seed) 
 np.random.seed(seed)#numpy产生的随机数一致
 random.seed(seed)
-
-# CUDA中的一些运算，如对sparse的CUDA张量与dense的CUDA张量调用torch.bmm()，它通常使用不确定性算法。
-# 为了避免这种情况，就要将这个flag设置为True，让它使用确定的实现。
 torch.backends.cudnn.deterministic = True
-# 设置这个flag可以让内置的cuDNN的auto-tuner自动寻找最适合当前配置的高效算法，来达到优化运行效率的问题。
-# 但是由于噪声和不同的硬件条件，即使是同一台机器，benchmark都可能会选择不同的算法。为了消除这个随机性，设置为 False
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.enabled = True
 
